848c65ef35b7c329b5573a1fc01662251144fcb0
choiiw <tjddnjs5245@naver.com>
2024-07-31 12:28:53 +0900

    수정 기능 완료

--- todolist_front/src/App.js
+++ todolist_front/src/App.js
@@ -1,28 +1,34 @@
 
 import './App.css';
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import MainContainer from './components/MainContainer/MainContainer';
 import MainLayout from './components/MainLayout/MainLayout';
 import { reset } from './styles/global';
 import axios from 'axios';
 import React, { useState } from 'react';
-
+import ReactModal from 'react-modal';
+ReactModal.setAppElement("#root");
 
 function App() {
-  // 조회
 
-    const [ todo, setTodo ] = useState({
+
+    const [ isModalOpen, setModalOpen ] = useState(false);
+    
+    const [ params, setParams ] = useState({
+        registerDate: "",
+    })
+        
+    const [ updateTodo, setUpdateTodo ] = useState({
         todoId: "",
         checkStatus: "",
         content: "",
         registerDate: "",
     });
+        
+        const [ todoList, setTodoList ] = useState([]);
+        
 
-    const [ params, setParams ] = useState({
-        registerDate: "",
-    })
-
-    const [ todoList, setTodoList ] = useState([]);
+      // 조회
 
     const requestTodoList = async () => {
         try{
@@ -45,43 +51,117 @@ function App() {
         });
 
     }
-// useEffect(() => {
-//     const getLists = async () => {
-//         const response = await axios.get("http://localhost:8080/api/v1/todolist");
-//         setTodoListOption(response.data);
-//         setTodoList(list => ({
-//             ...list,
-//             listId: response.data[0].listId
-//             })
-//         );
-//     }
-//     getLists();
-// }, []);
 
-
-// const handleSubmitClick = async () => {
-//     try {
-//                  const response = await axios.get("http://localhost:8080/api/v1/todolist", todoList);
-//                  setTodoListOption(response.data);
-//              }catch(e) {
-//                  console.error(e);
-//              };
-// }
+    const requestGetTodo = async (todoId) => {
+        let responseData = null;
+        try {
+            const response = await axios.get(`http://localhost:8080/api/v1/todo/${todoId}`);
+            console.log(response);
+            responseData = response.data;
+        } catch (e) {
+            console.error(e);
+        }
+        return responseData;
+    }
 
 // 생성
 
 // 삭제
 
 // 수정 
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setUpdateTodo({
+            todoId: "",
+            checkStatus: "",
+            content: "",
+            registerDate: "",
+        })
+    }
+
+    const handleUpdateTodoClick = async (todoId) => {
+
+        setModalOpen(true);
+        const data = await requestGetTodo(todoId); // x
+        setUpdateTodo(data);
+    }
+
+
+    const handleUpdateSubmitClick = async () => {
+        await requestUpdateTodo();
+        closeModal();
+    }
+
+    const requestUpdateTodo = async () => {
+        let responseData = null;
+
+        try {
+            const response = await axios.put(`http://localhost:8080/api/v1/todo/${updateTodo.todoId}`, updateTodo)
+            responseData = response.data;
+        } catch (e) {
+            console.error(e);
+        }
+        return responseData;
+    }
+
+    const handleUpdateInputChange = (e) => {
+        setUpdateTodo(ut => {
+            return {
+                ...ut,
+                [e.target.name]: e.target.value
+            };
+        });
+    }
+
+
   return (
     <>
+            <div>
+                <ReactModal 
+                    style={{
+                        content: {
+                            boxSizing: "border-box",
+                            transform: "translate(-50%, -50%)",
+                            top: "50%",
+                            left: "50%",
+                            padding: "20px",
+                            width: "800px",
+                            height: "400px",
+                            backgroundColor: "#fafafa",
+
+                        }
+                    }}
+                    isOpen={isModalOpen}
+                    onRequestClose={closeModal}
+                >
+                    <div css={css`
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                        align-items: center;
+                        height: 100%;
+                    `}>
+                        <h2>todo 정보 수정</h2>
+                        <input type="text" name="content" placeholder='오늘 할 일' onChange={handleUpdateInputChange} value={updateTodo.content} /> 
+                        <input type="date" name="registerDate" onChange={handleUpdateInputChange} value={updateTodo.registerDate} /> 
+                        <div>
+                            <button onClick={handleUpdateSubmitClick}>확인</button>
+                            <button onClick={() => closeModal()}>취소</button>
+                        </div>
+                    </div>
+                </ReactModal>
+            </div>
+
       <Global css={reset}/>
           <MainLayout>
             <MainContainer>
+
+
             <div >
             <div class="container">
                     <h1>todolist</h1>
-                <p class="input-box">
+                <p class="input-box">           
                     <input type="text" placeholder='ID'/>
                     <input type="password" placeholder='password'/>
                     <button class="login-bt">확인</button>
@@ -111,7 +191,7 @@ function App() {
                                         <td><input type="checkbox" checked={!!todo.checkStatus}/></td>
                                         <td>{todo.content}</td>
                                         <td>{todo.registerDate}</td>
-                                        <td><button>수정</button></td>
+                                        <td><button onClick={() => handleUpdateTodoClick(todo.todoId)}>수정</button></td>
                                         <td><button>삭제</button></td>
                                     </tr>
                      
