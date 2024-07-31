7360edd3d8b92464dc63033f236bc80943c54a5f
kim0705 <dbwls138@naver.com>
2024-07-31 16:41:01 +0900

    날짜별 조회

--- todolist_front/src/App.js
+++ todolist_front/src/App.js
@@ -1,206 +1,192 @@
-
 import './App.css';
 import { Global } from '@emotion/react';
 import MainContainer from './components/MainContainer/MainContainer';
 import MainLayout from './components/MainLayout/MainLayout';
 import { reset } from './styles/global';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
-  // 조회
+    // 조회
 
-    const [ todo, setTodo ] = useState({
-        todoId: "",
-        checkStatus: "",
-        content: "",
-        registerDate: "",
-    });
-
-    const [ params, setParams ] = useState({
+    const [params, setParams] = useState({
         registerDate: "",
     })
 
-    const [ todoList, setTodoList ] = useState([]);
+    const [todoList, setTodoList] = useState([]);
 
     const requestTodoList = async () => {
-        try{
-            const response = await axios.get("http://localhost:8080/api/v1/todolist", {params});
+        try {
+            const response = await axios.get("http://localhost:8080/api/v1/todolist", { params });
             setTodoList(response.data);
-        }catch(e) {
+        } catch (e) {
             console.error(e);
         };
     }
 
-    const handleSearchClick = () => {
-            
+    // const handleSearchClick = () => {
+
+    //     requestTodoList();
+
+    //     setParams({
+    //         registerDate: "",
+    //     })
+
+    // }
+
+    useEffect(() => {
+
         requestTodoList();
+        console.log(params);
 
-        setParams({
-            todoId: "",
-            checkStatus: "",
-            content: "",
-            registerDate: "",
-        });
+    }, [params]);
 
+    const handleInputChange = (e) => {
+
+        setParams(param => {
+            return {
+                ...param,
+                [e.target.name]: e.target.value
+            }
+        });  
     }
-// useEffect(() => {
-//     const getLists = async () => {
-//         const response = await axios.get("http://localhost:8080/api/v1/todolist");
-//         setTodoListOption(response.data);
-//         setTodoList(list => ({
-//             ...list,
-//             listId: response.data[0].listId
-//             })
-//         );
-//     }
-//     getLists();
-// }, []);
 
 
-// const handleSubmitClick = async () => {
-//     try {
-//                  const response = await axios.get("http://localhost:8080/api/v1/todolist", todoList);
-//                  setTodoListOption(response.data);
-//              }catch(e) {
-//                  console.error(e);
-//              };
-// }
+    // 생성
 
-// 생성
+    // 삭제
 
-// 삭제
+    // 수정 
+    return (
+        <>
+            <Global css={reset} />
+            <MainLayout>
+                <MainContainer>
+                    <div >
+                        <div class="container">
+                            <h1>todolist</h1>
+                            <p class="input-box">
+                                <input type='month' name='registerDate' onChange={handleInputChange} />
+                                <input type="text" placeholder='ID' />
+                                <input type="password" placeholder='password' />
+                                <button class="login-bt">확인</button>
+                            </p>
 
-// 수정 
-  return (
-    <>
-      <Global css={reset}/>
-          <MainLayout>
-            <MainContainer>
-            <div >
-            <div class="container">
-                    <h1>todolist</h1>
-                <p class="input-box">
-                    <input type="text" placeholder='ID'/>
-                    <input type="password" placeholder='password'/>
-                    <button class="login-bt">확인</button>
-                </p>
+                            <div class="list-container">
+                                <div class="list">
+                                    <div class="sc-box">
+                                        <h2>전체 List</h2>
+                                    </div>
+                                    <div >
+                                        <table>
+                                            <thead>
+                                                <tr >
+                                                    <th>선택</th>
+                                                    <th>내용</th>
+                                                    <th>날짜</th>
+                                                    <th>수정</th>
+                                                    <th>삭제</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {
+                                                    todoList.map(todo =>
+                                                        <tr key={todo.todoId}>
+                                                            <td><input type="checkbox" checked={!!todo.checkStatus} /></td>
+                                                            <td>{todo.content}</td>
+                                                            <td>{todo.registerDate}</td>
+                                                            <td><button>수정</button></td>
+                                                            <td><button>삭제</button></td>
+                                                        </tr>
 
-                <div class="list-container">
-                    <div class="list">
-                    <div class="sc-box">
-                    <h2>전체 List</h2>
-                </div>
-                <div >
-                    <table>
-                        <thead>
-                            <tr >
-                                <th>선택</th>
-                                <th>내용</th>
-                                <th>날짜</th>
-                                <th>수정</th>
-                                <th>삭제</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                           {
-                                todoList.map(todo =>
-                                    <tr key={todo.todoId}>
-                                        <td><input type="checkbox" checked={!!todo.checkStatus}/></td>
-                                        <td>{todo.content}</td>
-                                        <td>{todo.registerDate}</td>
-                                        <td><button>수정</button></td>
-                                        <td><button>삭제</button></td>
-                                    </tr>
-                     
-                                )    
-                             }
-                        </tbody>    
-                    </table>
-                </div>
+                                                    )
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div>
 
+                                </div>
+                                <div class="list">
+                                    <div class="sc-box">
+                                        <h2>미완료 List</h2>
+                                    </div>
+                                    <div >
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>선택</th>
+                                                    <th>내용</th>
+                                                    <th>날짜</th>
+                                                    <th>수정</th>
+                                                    <th>삭제</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                                {
+                                                    todoList.filter(todo => !todo.checkStatus).map(todo =>
+                                                        <tr key={todo.todoId}>
+                                                            <td><input type="checkbox" checked={!!todo.checkStatus} /></td>
+                                                            <td>{todo.content}</td>
+                                                            <td>{todo.registerDate}</td>
+                                                            <td><button>수정</button></td>
+                                                            <td><button>삭제</button></td>
+                                                        </tr>
+
+                                                    )
+                                                }
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                                <div class="list">
+                                    <div class="sc-box">
+                                        <h2>완료 List</h2>
+                                    </div>
+                                    <div >
+                                        <table>
+                                            <thead>
+                                                <tr >
+                                                    <th>선택</th>
+                                                    <th>내용</th>
+                                                    <th>날짜</th>
+                                                    <th>수정</th>
+                                                    <th>삭제</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                                {
+                                                    todoList.filter(todo => !!todo.checkStatus).map(todo =>
+                                                        <tr key={todo.todoId}>
+                                                            <td><input type="checkbox" checked={!!todo.checkStatus} /></td>
+                                                            <td>{todo.content}</td>
+                                                            <td>{todo.registerDate}</td>
+                                                            <td><button>수정</button></td>
+                                                            <td><button>삭제</button></td>
+                                                        </tr>
+
+                                                    )
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="list">
-                    <div class="sc-box">
-            <h2>미완료 List</h2>
-        </div>
-        <div >
-            <table>
-                <thead>
-                    <tr>
-                        <th>선택</th>
-                        <th>내용</th>
-                        <th>날짜</th>
-                        <th>수정</th>
-                        <th>삭제</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    {
-                        todoList.filter(todo => !todo.checkStatus).map(todo =>
-                            <tr key={todo.todoId}>
-                                <td><input type="checkbox" checked={!!todo.checkStatus}/></td>
-                                <td>{todo.content}</td>
-                                <td>{todo.registerDate}</td>
-                                <td><button>수정</button></td>
-                                <td><button>삭제</button></td>
-                            </tr>
-                
-                        )    
-                    }
 
-                </tbody>    
-            </table>
-        </div>
-  
-                    </div>
-                    <div class="list">
-                    <div class="sc-box">
-                    <h2>완료 List</h2>
-                </div>
-                <div >
-                    <table>
-                        <thead>
-                            <tr >
-                                <th>선택</th>
-                                <th>내용</th>
-                                <th>날짜</th>
-                                <th>수정</th>
-                                <th>삭제</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        
-                        {
-                            todoList.filter(todo => !!todo.checkStatus).map(todo =>
-                                <tr key={todo.todoId}>
-                                    <td><input type="checkbox" checked={!!todo.checkStatus}/></td>
-                                    <td>{todo.content}</td>
-                                    <td>{todo.registerDate}</td>
-                                    <td><button>수정</button></td>
-                                    <td><button>삭제</button></td>
-                                </tr>
-                    
-                            )    
-                        }
-                        </tbody>    
-                    </table>
-                </div>
+                </MainContainer>
 
-                    </div> 
-                    
-                </div>
-
-            </div>
-       </div>
-
-            </MainContainer>
-
-          </MainLayout>
-    </>
-  );
+            </MainLayout>
+        </>
+    );
 }
 
 export default App;
