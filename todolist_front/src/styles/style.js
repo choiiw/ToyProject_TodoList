bb83220a1ab48b31f620e6048cd80759d9e08588
kim0705 <dbwls138@naver.com>
2024-08-05 12:23:59 +0900

    스크롤 수정

---
+++ todolist_front/src/styles/style.js
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const h2 = css`
+    padding: 6px;
+    margin-left: 30px;
+    border-bottom: 4px solid #D6E0F0;
+    color: #a3c2e5;
+    font-family: 'GangwonEduSaeeum_OTFMediumA';
+    font-size: 60px;
+    text-shadow: 1px 1px 0 #000000, 1px -1px 0 #000000, 1px -1px 0 #000000,-1px 1px 0 #000000;
+    align-items: center;
+`; 
